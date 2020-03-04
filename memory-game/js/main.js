cc5b0f1c149f279193fd6dc32eeba4248e959418
mbpod10 <mbpod10@gmail.com>
2020-03-03 21:56:38 -0500

    created game logic w/ functions

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -4,7 +4,38 @@ console.log("Up and running!");
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
+
+function checkForMath(){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+			console.log("You found a match!");
+		}else{
+			console.log("Sorry try again.");
+			}
+};
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]); // IN CONSOLE: flipCard(0) = 'User flipped queen'; flipcard(2) = 'User flipped king';
+	cardsInPlay.push(cards[cardId]); 			 // push cardId into cardsInPlay array
+};
+
+
+// Sequence
+// console: flipCard(0) = 'User flipped queen'
+// console: flipcard(2) = 'User flipped king'
+// console: checkForMath() = "Sorry try again."
+
+// console: flipCard(0) = 'User flipped queen'
+// console: flipcard(1) = 'User flipped queen'
+// console: checkForMath() = "You found a match." 
+
+
+
+
+
+
+
+
+/*let cardOne = cards[0];
 cardsInPlay.push("cardOne");
 console.log("User flipped: " + cardOne);
 
@@ -18,4 +49,4 @@ if(cardsInPlay.length === 2){
 }else{
 	alert("Sorry try again.");
 	}
-}
+}*/
